cfe74b9548b9fd05f05252e55faddf3c167925ec
Akshat <iamakku0.0.0.1@gmail.com>
2024-08-29 21:13:13 +0530

    added prometheus configuration and metrics route

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const { doSomeHeavyTask } = require('./util.js');
 const express = require("express");
+//importing prom-client
+const promClient = require('prom-client');
 
 const app = express();
+
+//step 1 Prometheus
+//collecting default metrics of our server
+const collectDefaultMetrics = promClient.collectDefaultMetrics;
+//registering it
+collectDefaultMetrics({register:promClient.register});
 
 
 const port = 8000;
@@ -21,6 +29,17 @@ app.get('/slow',async(req,res)=>{
     }catch(error){
         return res.status(500).json({error:"Internal server errror"});
     }
+})
+
+//step 2 Prometheus
+//api to provide all collected metrics
+app.get('/metrics',async(req,res)=>{
+//we had set the content type into headers
+ res.setHeader('Content-Type',promClient.register.contentType);
+ //get the metrics from promp client
+ const metrics = await promClient.register.metrics();
+ //return the metrics in reponse
+ res.send(metrics);
 })
 
 app.listen(port,()=>{
