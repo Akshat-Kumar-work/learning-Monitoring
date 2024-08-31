095144efe7574d3e9501f27e6fe28d527f2830a0
Akshat <iamakku0.0.0.1@gmail.com>
2024-08-31 23:20:38 +0530

    exposed metrics route

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import express from "express";
+import { reqCount } from "./custom-metrics/requestCount";
+import client from "prom-client";
 
 const app = express();
 
 app.use(express.json());
+
+//using custom middleware
+app.use(reqCount);
 
 app.get("/user", (req, res) => {
     res.send({
@@ -18,5 +23,18 @@ app.post("/user", (req, res) => {
         id: 1,
     });
 });
+
+//exposing metrics endpoint
+app.get('/metrics',async(req,res)=>{
+
+ //setting header name as Content-Type and value
+ //  (client.register.contentType)=> it is used to get the current metrics from prom client registry
+    res.setHeader('Content-Type',client.register.contentType);
+
+    //get the registered metrics from prom client
+    const metrics = await client.register.metrics();
+    
+    res.send(metrics);
+})
 
 app.listen(3000);
